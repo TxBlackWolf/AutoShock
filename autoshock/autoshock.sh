543ec87db71677222539598275f805a67aabc57c
KuteKetX <38116775+KuteKetX@users.noreply.github.com>
2018-07-07 13:56:58 -0600

    Create autoshock.sh
    i havent started writing the script yet, im still working on finding exploits.

---
+++ autoshock/autoshock.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+################################################################################
+#                                   AutoShock                                  #
+#                                  By KuteKetX                                 #
+#               Used for the automation of exploiting shellshock               #
+################################################################################
+# CVE-2014-6277                                                                #
+# CVE-2014-6278                                                                #
+# CVE-2014-7169                                                                #
+# CVE-2014-7186                                                                #
+# CVE-2014-7187                                                                #
+# CVE-2014-6271                                                                #
+#------------------------------------------------------------------------------#
+# https://en.wikipedia.org/wiki/Shellshock_(software_bug)                      #
+# https://www.us-cert.gov/ncas/alerts/TA14-268A                                #
+# https://www.owasp.org/images/1/1b/Shellshock_-_Tudor_Enache.pdf              #
+# https://www.rapid7.com/db/vulnerabilities/gnu-bash-cve-2014-6278             #
+# https://github.com/opsxcq/exploit-CVE-2014-6271                              #
+# https://cve.mitre.org/cgi-bin/cvename.cgi?name=cve-2014-6271                 #
+# https://www.cvedetails.com/cve/cve-2014-6278                                 #
+################################################################################
+#                                   EXPLOITS                                   #
+#   Metasploit:                                                                #
+# apache_mod_cgi_bash_env_exec                                                 #
+# Bash CGI - 'Shellshock' Remote Command Injection                             #
+#------------------------------------------------------------------------------#
+#   ExploitDB:                                                                 #
+# Apache mod_cgi - 'Shellshock' Remote Command Injection (34900)               #
+# Bash - 'Shellshock' Environment Variables Command Injection (34766)          #
+################################################################################
